b23894ee0b8a716f3662bd4758b3fe62820a54c3
David <davidread1998@gmail.com>
2021-11-13 19:05:24 -0500

    Add a model class for an article listing
    Article.java:
    - Define a model class to represent an article listing.
    - Each article object has a title, a section name, a date published, and a URL.

---
+++ app/src/main/java/com/davidread/newsfeed/Article.java
@@ -0,0 +1,78 @@
+package com.davidread.newsfeed;
+
+/**
+ * {@link Article} is a model class for an article listing.
+ */
+public class Article {
+
+    /**
+     * {@link String} representing the title of the article.
+     */
+    private String title;
+
+    /**
+     * {@link String} representing the name of the section the article is from.
+     */
+    private String sectionName;
+
+    /**
+     * {@link String} representing the date the article was published.
+     */
+    private String datePublished;
+
+    /**
+     * {@link String} representing a URL that points to the article online.
+     */
+    private String url;
+
+    /**
+     * Constructs a new {@link Article} object.
+     *
+     * @param title         {@link String} representing the title of the article.
+     * @param sectionName   {@link String} representing the name of the section the article is from.
+     * @param datePublished {@link String} representing the date the article was published.
+     * @param url           {@link String} representing a URL that points to the article online.
+     */
+    public Article(String title, String sectionName, String datePublished, String url) {
+        this.title = title;
+        this.sectionName = sectionName;
+        this.datePublished = datePublished;
+        this.url = url;
+    }
+
+    /**
+     * Returns a {@link String} representing the title of the article.
+     *
+     * @return {@link String} representing the title of the article.
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * Returns a {@link String} representing the name of the section the article is from.
+     *
+     * @return {@link String} representing the name of the section the article is from.
+     */
+    public String getSectionName() {
+        return sectionName;
+    }
+
+    /**
+     * Returns a {@link String} representing the date the article was published.
+     *
+     * @return {@link String} representing the date the article was published.
+     */
+    public String getDatePublished() {
+        return datePublished;
+    }
+
+    /**
+     * Returns a {@link String} representing a URL that points to the article online.
+     *
+     * @return {@link String} representing a URL that points to the article online.
+     */
+    public String getUrl() {
+        return url;
+    }
+}
